dfb82cb4f5248468ceb37b977d503b8edab967c6
mkoroleva5 <mkoroleva5@gmail.com>
2023-03-13 21:16:31 +0300

    feat: add about page

---
+++ module-01-components/src/components/AboutPageComponent/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class AboutPage extends React.Component {
+  constructor(props: object) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+      </div>
+    );
+  }
+}
