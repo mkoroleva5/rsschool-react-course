944dcba6f9cc7a0dc40b0ea17823e4e6961ca912
mkoroleva5 <mkoroleva5@gmail.com>
2023-03-13 21:16:24 +0300

    feat: add home page

---
+++ module-01-components/src/components/HomePageComponent/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class HomePage extends React.Component {
+  constructor(props: object) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
