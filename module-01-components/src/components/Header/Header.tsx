d190e6fc0380da85a3ce2362f878b33b0b2d628f
mkoroleva5 <mkoroleva5@gmail.com>
2023-03-15 15:15:35 +0300

    fix: fix card and header styles

--- module-01-components/src/components/Header/Header.tsx
+++ module-01-components/src/components/Header/Header.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import catLogo from '../../assets/icons/cat-icon.png';
 
 export class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <NavLink className="link" to="/">
-          Home
+        <NavLink className="header__link" to="/">
+          <img className="header__logo" src={catLogo} alt="Cat" />
         </NavLink>
-        <NavLink className="link" to="/about">
-          About Us
-        </NavLink>
+        <nav className="header__nav">
+          <NavLink className="header__link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="header__link" to="/about">
+            About Us
+          </NavLink>
+        </nav>
       </header>
     );
   }
