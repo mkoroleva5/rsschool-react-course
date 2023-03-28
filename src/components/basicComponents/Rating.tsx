87725d33abe4b24b036fb7c7ac4c5596ba23aa39
mkoroleva5 <mkoroleva5@gmail.com>
2023-03-28 15:58:10 +0300

    feat: rewite rating component using hooks

--- src/components/basicComponents/Rating.tsx
+++ src/components/basicComponents/Rating.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Rating.css';
 
 interface RatingProps {
@@ -6,21 +5,15 @@ interface RatingProps {
   size: number;
 }
 
-export class Rating extends React.Component<RatingProps> {
-  render() {
-    return (
-      <div className="heart-ratings" style={{ fontSize: `${this.props.size}px` }}>
-        <div
-          className="fill-ratings"
-          data-testid="rating"
-          style={{ width: `${this.props.cuteness}%` }}
-        >
-          <span>♥♥♥♥♥</span>
-        </div>
-        <div className="empty-ratings">
-          <span>♥♥♥♥♥</span>
-        </div>
+export const Rating = ({ cuteness, size }: RatingProps) => {
+  return (
+    <div className="heart-ratings" style={{ fontSize: `${size}px` }}>
+      <div className="fill-ratings" data-testid="rating" style={{ width: `${cuteness}%` }}>
+        <span>♥♥♥♥♥</span>
       </div>
-    );
-  }
-}
+      <div className="empty-ratings">
+        <span>♥♥♥♥♥</span>
+      </div>
+    </div>
+  );
+};
