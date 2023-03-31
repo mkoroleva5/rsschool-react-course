1c1daaea502a2a66df5a39cf92ef8773588c2295
mkoroleva5 <mkoroleva5@gmail.com>
2023-03-31 22:32:14 +0300

    fix: adjust progress bar

--- src/components/basicComponents/ProgressBar.tsx
+++ src/components/basicComponents/ProgressBar.tsx
@@ -3,7 +3,7 @@ import './ProgressBar.css';
 export const ProgressBar = () => {
   return (
     <div className="progress-bar_wrapper">
-      In progress...
+      Progressing...
       <div className="progress-bar">
         <div className="progress-bar_inner"></div>
       </div>
