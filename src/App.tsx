0e42f2fb9bcf08ecc5c502baa5562d2d737d9a4f
mkoroleva5 <mkoroleva5@gmail.com>
2023-04-20 21:34:36 +0300

    fix: remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { HomePage } from './components/HomePage/HomePage';
 import { CatsPage } from './components/CatsPage/CatsPage';
